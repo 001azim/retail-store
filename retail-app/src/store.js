922ca84b55d88ae611c241cc29eb355401a67aae
Abishamuthu <abishamuthukrishnan2003@gmail.com>
2023-12-19 15:13:18 +0530

    register

--- retail-app/src/store.js
+++ retail-app/src/store.js
@@ -3,13 +3,7 @@ import customerSlice from './slices/customerSlice'
 import registerSlice from './slices/registerSlice'
 export default configureStore({
   reducer: {
-<<<<<<< HEAD
-
-customer:customerSlice
-
-=======
   customer:customerSlice,
->>>>>>> 4e1bd253806641199ed5d7d28c159a22e7916a86
   },
   reducer: {
     register:registerSlice,
