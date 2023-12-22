98e706cc96b4e1be20e8b8a3021768ffa5f9dd1d
ajilj4 <ajilj4@gmail.com>
2023-12-22 11:27:56 +0530

    new commit

--- retail-app/src/shop-owner/cus-list.js
+++ retail-app/src/shop-owner/cus-list.js
@@ -1,108 +1,95 @@
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from 'axios';
-
+import Form from 'react-bootstrap/Form';
 import "../css/cus-list.css"
-import { useState,useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Common from '../components/common';
-
+import InputGroup from 'react-bootstrap/InputGroup';
 
 
 function CUSTOMERLST() {
 
-  let [apidata,setapidata] = useState([])
-
-  const navigate= useNavigate()
+  let [apidata, setapidata] = useState([])
+  
+  const [query, setQuery] = useState("")
+   
+  const navigate = useNavigate()
   // const home = () =>navigate("/")
   // const back = ()=>window.history.back()
   // const forward = () => window.history.forward
 
-  const add = ()=>{
+  const add = () => {
     alert("sucess")
   }
-  
- 
 
-  
-   
-useEffect(()=>{
-  axios({
-    method: 'get',
-    url: 'https://jsonplaceholder.typicode.com/users',
-    
-  })
-    .then(function(res){
-      setapidata(res.data)
+  const filteredItems = useMemo(() => {
+    return apidata.filter(item => {
+      return item.name.toLowerCase().includes(query.toLowerCase())
     })
+  }, [apidata, query])
 
-},[])
 
 
-console.log(apidata)
-  // axios({
-  //   method: 'get',
-  //   url: 'https://jsonplaceholder.typicode.com/users',
-    
-  // })
-  //   .then(async function (response) {
-  //     // console.log(response.data)
-  //     let data = response.data;
-  //     let htmll='';
-  //     // let custm= data.map((s)=>{
-  //     //   htmll=htmll+
-  //     // })
-  //     for (let i=0;i<data.length;i++){
-  //     //  console.log(data[i].phone_number)
-  //      htmll= htmll+`<tr>
-  //       <td>${data[i].id}</td>
-  //       <td>${data[i].customer_name}</td>
-  //       <td>${data[i].phone_number}</td>
-  //       <td>${data[i].date_of_last_purchase}</td>
-  //       <td>${data[i].debt_amount}</td>
-  //       <td>${data[i].due_date}</td>
-  //       <td><Button variant="success" onClick={}>Success</Button></td>
-  //     </tr>`
-     
-  //     }
-  //     document.getElementById("listdata").innerHTML= htmll
-  //   });
-    
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'https://jsonplaceholder.typicode.com/users',
+
+    })
+      .then(function (res) {
+        setapidata(res.data)
+        
+      })
+
+  }, [])
 
   return (
     <>
-    <Common />
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Contact</th>
-          <th>Purchase date</th>
-          <th>debt_amount</th>
-          <th>due_date</th>
-          <th>Send Notification</th>
-        </tr>
-      </thead>
-      <tbody >
-        
-          {apidata.map((s)=>{
-           return(
-              <tr>
-                <td>{s.id}</td>
-                <td>{s.name}</td>
-                <td>{s.username}</td>
-                {/* <td>{s}</td> */}
-                {/* <td><button>delete</button></td> */}
+      <Common />
+      <div className='boxs'>
+      <div className='form-flex'>
+        <div className='left-form'>
+        <Form>
+        <InputGroup className='my-3 search'>
+        <Form. Control placeholder='Search contacts' value={query} onChange={e => setQuery(e.target.value)} />
+        </InputGroup>
+        </Form>
+        </div>
+        <div className='right-form d-flex'>
+        <Button variant="success" onClick={() => navigate('/creditordebit')} > Add Customer</Button>
+        </div>
+        </div>
+
+      <Table responsive striped bordered hover variant="light" className='cus-table'>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Contact</th>
+            <th>Purchase date</th>
+            <th>debt_amount</th>
+            <th>due_date</th>
+            <th>Send Notification</th>
+          </tr>
+        </thead>
+        <tbody >
+          
+              {filteredItems.map((item,i) => (
+              <tr key={i}>
+                
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.username}</td>
+                
               </tr>
-          )
-})}
-       
-      </tbody>
-    </Table>
-    <Button  variant="success" onClick={()=> navigate('/creditordebit')} > Add Customer</Button>
-    
-    
+            ))}
+
+        </tbody>
+      </Table>
+      {/* <Button variant="success" onClick={() => navigate('/creditordebit')} > Add Customer</Button> */}
+   </div>
     </>
   );
 }
