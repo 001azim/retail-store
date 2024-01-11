778f95424456d92128a4501837e887697f88b245
Abishamuthu <abishamuthukrishnan2003@gmail.com>
2024-01-11 10:15:08 +0530

    latest

--- retail-app/src/App.js
+++ retail-app/src/App.js
@@ -31,7 +31,7 @@ function App() {
     {
       path: "/msg",
       element: <MESSAGELST />
-    },
+    },                                                                                                                                              
     {
       path: "/addcustomer",
       element: <ADDAMOUNT />
